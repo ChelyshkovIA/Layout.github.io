7b4820d211da1f65205aa5e6fce1a56520c5dc71
Ilya <great.mars15@gmail.com>
2020-04-15 00:57:52 +0300

    update styles

--- GreenWich/JS/index.js
+++ GreenWich/JS/index.js
@@ -5,10 +5,18 @@ window.addEventListener('DOMContentLoaded', function() {
 	let mobileMenu = document.querySelector('.mobile__menu');
 
 	burgerMenu.addEventListener('click', function() {
+		document.querySelector('main').classList.add('blured');
+		document.querySelector('footer').classList.add('blured');
+		document.body.classList.add('overflowHidden');
+		document.querySelector('.burger').classList.add('hidden');
 		mobileMenu.classList.remove('menu--closed');
 	});
 
 	menuClose.addEventListener('click', function() {
+		document.querySelector('main').classList.remove('blured');
+		document.querySelector('footer').classList.remove('blured');
+		document.body.classList.remove('overflowHidden');
+		document.querySelector('.burger').classList.remove('hidden');
 		mobileMenu.classList.add('menu--closed');
 	});
 });
