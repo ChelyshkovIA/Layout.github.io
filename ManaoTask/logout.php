92e17dfd2fbc5ee212f5918bed62e331e1424940
Ilya <great.mars15@gmail.com>
2020-08-07 00:05:51 +0300

    delete manao tasks from github.io

--- ManaoTask/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    setcookie('user', $login, time() - 3600, '/');
-    setcookie('hash', $hash, time() - 3600, '/');
-    session_start();
-    $_SESSION = array();
-    session_destroy();
-	header("Location: index.html");
-?>
